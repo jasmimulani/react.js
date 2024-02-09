22fbaa5b89087de97b5c32eb1c69a723508fc428
Jasmin <jasmimulani@gmail.com>
2024-02-09 20:52:27 +0530

    09/02/24

--- javascript/lecture-10/lecture-10.js
+++ javascript/lecture-10/lecture-10.js
@@ -28,13 +28,162 @@ new Date(year,month,day,hours,minutes)
 new Date(year,month,day,hours,minutes,seconds)
 new Date(milliseconds)
 
+
+
 */
 
 // let Date1 = new Date();
 // console.log(Date1);
 
+
+// new Date(date string)
 // let date2 = new Date("10-12-2050")
 // console.log(date2);
 
+
+// new Date(year,month)
 // let date3 = new Date(2023 , 4);
 // console.log(date3);
+
+// new Date(year,month,day)
+// let date4 = new Date(2023 , 1 , 26)
+// console.log(date4);
+
+
+// new Date(year,month,day,hours)
+// let date5 = new Date (2023 , 1 , 26 , 16)
+// console.log(date5);
+
+// new Date(year,month,day,hours,minutes)
+// let date6 = new Date(2023 , 4 , 26 , 16 , 12)
+// console.log(date6);
+
+// new Date(year,month,day,hours,minutes,seconds)
+// let date7 = new Date (2023 , 4 , 26 , 16 , 12 , 59)
+// console.log(date7);
+
+// new Date(milliseconds) contdown start with 1 jan 1970
+// let date8 = new Date(120000)
+// console.log(date8);
+
+
+/* javascript ISO standard time */
+{
+    // const dates = new Date(2023 , 3 , 16 , 6 , 15 , 20)
+    // console.log(dates);
+    // console.log(dates.toString());
+    // console.log(dates.toDateString());
+    // console.log(dates. toUTCString());
+    // console.log(dates.toISOString());
+}
+
+/* JavaScript Date Formats */
+
+/* ISO Date , Short Date  , Long Date */
+{
+    // const dates = new Date("2024");
+    // console.log(dates);
+}
+
+/* ShortDate */
+{
+    // const dates = new Date("03/25/2023")
+    // console.log(dates);
+}
+
+/* LongDate */
+{
+    // const dates = new Date("march 23 2023")
+    // console.log(dates);
+}
+
+{
+//     const dates = new Date("23 march 2023");
+//     console.log(dates);
+}
+
+/*  Date Input - Parsing Dates */
+{
+    // let msec = Date.parse("october 21 , 2002");
+    // console.log(msec);
+}
+
+
+/* JavaScript Get Date Methods */
+
+/*
+
+getFullYear()	Get year as a four digit number (yyyy)
+getMonth()	Get month as a number (0-11)
+getDate()	Get day as a number (1-31)
+getDay()	Get weekday as a number (0-6)
+getHours()	Get hour (0-23)
+getMinutes()	Get minute (0-59)
+getSeconds()	Get second (0-59)
+getMilliseconds()	Get millisecond (0-999)
+getTime()	Get time (milliseconds since January 1, 1970)
+
+*/
+
+// let date1 = new Date
+// console.log(date1);
+
+// let date2 = date1.getFullYear();
+// console.log(date2);
+
+// let date3 = date1.getMonth();
+// console.log(date3);
+
+// let date4 = date1.getDate();
+// console.log(date4);
+
+// let date5 = date1.getDay();
+// console.log(date5);
+
+// let date6 = date1.getHours();
+// console.log(date6);
+
+// let date7 = date1.getMinutes();
+// console.log(date7);
+
+// let date8 = date1.getSeconds();
+// console.log(date8);
+
+// let date9 = date1.getMilliseconds();
+// console.log(date9);
+
+// let date10 = date1.getTime();
+// console.log(date10);
+
+
+
+/*  JavaScript Set Date Methods */
+
+/*
+
+setDate()	Set the day as a number (1-31)
+setFullYear()	Set the year (optionally month and day)
+setHours()	Set the hour (0-23)
+setMilliseconds()	Set the milliseconds (0-999)
+setMinutes()	Set the minutes (0-59)
+setMonth()	Set the month (0-11)
+setSeconds()	Set the seconds (0-59)
+setTime()	Set the time (milliseconds since January 1, 1970)
+
+*/
+
+// let date1 = new Date();
+// console.log(date1);
+
+// let date2 = date1.setFullYear(2020)
+// console.log(date2);
+// console.log(date1);
+
+// let date3 = date1.setDate(29)
+// console.log(date1);
+
+// let date4 = date1.setHours(45)
+// console.log(date1);
+
+// let date5 = date1.setMonth(12)
+// console.log(date1);
---
+++ javascript/lecture-11/lecture-11.js
@@ -0,0 +1,18 @@
+/* Javascript Array */  
+
+// let newaary = [1,false,"string"]
+// console.log(newaary);
+// console.log(newaary.length);
+
+// let newarry2 = ["apple", "banana", "watermelon", , "lichi"]   // array maa koy jagiya khali hoy pn teni index value hoy tene array no void element
+// console.log(newarry2);
+// console.log(newarry2[4]);
+// console.log(Object.keys(newarry2));
+
+
+// newarry2[0] = "graps"
+// console.log(newarry2);
+
+// let newstring = newarry2.toString();
+// console.log(newstring);
+// console.log(typeof(newstring));
