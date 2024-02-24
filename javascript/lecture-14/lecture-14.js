905be455413bc2617f6d63b15724c624a7047ce6
Jasmin <jasmimulani@gmail.com>
2024-02-24 18:42:03 +0530

    24/02/24

--- javascript/lecture-14/lecture-14.js
+++ javascript/lecture-14/lecture-14.js
@@ -34,45 +34,207 @@
 
 // console.log(dataobject3["marks"]["opreatingsystem"]);
 
-const dataobject4 = {
-    array:[1,2,3,4,5,6,[111,222]],
-    array2:[11,12,13,14,15,16],
-}
+// const dataobject4 = {
+//     array:[1,2,3,4,5,6,[111,222]],
+//     array2:[11,12,13,14,15,16],
+// }
 
-console.log(dataobject4.array);
-console.log(dataobject4.array[6][0]);
+// console.log(dataobject4.array);
+// console.log(dataobject4.array[6][0]);
 
 
-const array1 = [
-    {
-    id:1,
-    item:"apple",
-    catagories:"fruits"
-},
+// const array1 = [
+//     {
+//     id:1,
+//     item:"apple",
+//     catagories:"fruits"
+// },
+
+// {
+//     id:2,
+//     item:"banana",
+//     catagories:"fruits"
+//   },
+
+//   {
+//     id:3,
+//     item:"graps",
+//     catagories:"fruits"
+//   },
+
+//   {
+//     id:4,
+//     item:"mango",
+//     catagories:"fruits"
+//   },
+
+//   {
+//     id:5,
+//     item:"watermelon",
+//     catagories:"fruits"
+//   }
+// ]
+
+//  console.log(array1[0].id);
+
+
+// let arraay= [5,10,15,20,25,30,35,40,45,50,60,70,80,90,100];
+// let array1 = arraay.filter(num => num <= 50)
+// console.log(array1);
+// let array2 = arraay.filter(num => num >= 50)
+// console.log(array2);
+
 
 {
-    id:2,
-    item:"banana",
-    catagories:"fruits"
-  },
 
-  {
-    id:3,
-    item:"graps",
-    catagories:"fruits"
-  },
+const list = [{
+        fruits:
+        {
+            fruits1:{
 
-  {
-    id:4,
-    item:"mango",
-    catagories:"fruits"
-  },
+                name1:"apple",
+            },
+            fruits2:{
 
-  {
-    id:5,
-    item:"watermelon",
-    catagories:"fruits"
-  }
+                name2:"banana",
+            },
+            fruits3:{
+
+                name3:"cheryy",
+            },
+            fruits4:{
+
+                name4:"strobery",
+            },
+            fruits5:{
+
+                name5:"orange",
+            }
+        },
+        fastfood:
+        {
+            fastfood1:
+            {
+               item1:"panipuri",
+            },
+            fastfood2:
+            {
+               item2:"manchausup",
+            },
+            fastfood3:
+            {
+               item3:"pawbhaji",
+            },
+            fastfood4:
+            {
+               item4:"sendwich",
+            },
+            fastfood5:
+            {
+               item5:"dosa",
+            }
+        },
+        color:
+        {
+            color1:
+            {
+               color1:"red",
+            },
+            color2:
+            {
+               color2:"blue",
+            },
+            color3:
+            {
+               color3:"yellow",
+            },
+            color4:
+            {
+               color:"green",
+            },
+            color5:
+            {
+               color5:"black",
+            }
+        },
+        subtv:
+        {
+            subtv1:
+            {
+               person1:"jethalal",
+            },
+            subtv2:
+            {
+                person2:"dayabhabhi",
+            },
+            subtv3:
+            {
+                person3:"babitaji",
+            },
+            subtv4:
+            {
+                person4:"champkchacha",
+            },
+            subtv5:
+            {
+                person5:"dr.hathi",
+            }
+        },
+        cartoon:
+        {
+            cartoon1:
+            {
+               c1:"chotabheem",
+            },
+            cartoon2:
+            {
+               c2:"raju",
+            },
+            cartoon3:
+            {
+               c3:"indumati",
+            },
+            cartoon4:
+            {
+               c4:"tuntunmosi",
+            },
+            cartoon5:
+            {
+               c5:"kaliya",
+            }
+        },
+    }
 ]
 
- console.log(array1[0].id);
+ const input = prompt("enter fruits or items:");
+
+ if(input == " fruits"){
+    console.log("fruits",list[0]["fruits"]);
+ }
+ else if
+    (input == "fastfood"){
+        console.log("fastfood",list[0]["fastfood"]);
+ }
+ else if
+    (input == "color"){
+        console.log("color",list[0]["color"]);
+ }
+ else if
+    (input == "subtv"){
+        console.log("subtv",list[0]["subtv"]);
+ }
+ else if
+    (input == "cartoon"){
+        console.log("cartoon",list[0]["cartoon"]);
+ }
+    else{
+        console.log("please! page is not found");
+    }
+}    
+    
+
+
+
+
+
+
