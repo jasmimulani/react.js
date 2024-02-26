7b796d4abb0248f73218a1263a1a8e3082792031
Jasmin <jasmimulani@gmail.com>
2024-02-26 17:08:36 +0530

    26/02/24

--- javascript/lecture-14/lecture-14.js
+++ javascript/lecture-14/lecture-14.js
@@ -85,152 +85,152 @@
 // console.log(array2);
 
 
-{
+// {
 
-const list = [{
-        fruits:
-        {
-            fruits1:{
+// const list = [{
+//         fruits:
+//         {
+//             fruits1:{
 
-                name1:"apple",
-            },
-            fruits2:{
+//                 name1:"apple",
+//             },
+//             fruits2:{
 
-                name2:"banana",
-            },
-            fruits3:{
+//                 name2:"banana",
+//             },
+//             fruits3:{
 
-                name3:"cheryy",
-            },
-            fruits4:{
+//                 name3:"cheryy",
+//             },
+//             fruits4:{
 
-                name4:"strobery",
-            },
-            fruits5:{
+//                 name4:"strobery",
+//             },
+//             fruits5:{
 
-                name5:"orange",
-            }
-        },
-        fastfood:
-        {
-            fastfood1:
-            {
-               item1:"panipuri",
-            },
-            fastfood2:
-            {
-               item2:"manchausup",
-            },
-            fastfood3:
-            {
-               item3:"pawbhaji",
-            },
-            fastfood4:
-            {
-               item4:"sendwich",
-            },
-            fastfood5:
-            {
-               item5:"dosa",
-            }
-        },
-        color:
-        {
-            color1:
-            {
-               color1:"red",
-            },
-            color2:
-            {
-               color2:"blue",
-            },
-            color3:
-            {
-               color3:"yellow",
-            },
-            color4:
-            {
-               color:"green",
-            },
-            color5:
-            {
-               color5:"black",
-            }
-        },
-        subtv:
-        {
-            subtv1:
-            {
-               person1:"jethalal",
-            },
-            subtv2:
-            {
-                person2:"dayabhabhi",
-            },
-            subtv3:
-            {
-                person3:"babitaji",
-            },
-            subtv4:
-            {
-                person4:"champkchacha",
-            },
-            subtv5:
-            {
-                person5:"dr.hathi",
-            }
-        },
-        cartoon:
-        {
-            cartoon1:
-            {
-               c1:"chotabheem",
-            },
-            cartoon2:
-            {
-               c2:"raju",
-            },
-            cartoon3:
-            {
-               c3:"indumati",
-            },
-            cartoon4:
-            {
-               c4:"tuntunmosi",
-            },
-            cartoon5:
-            {
-               c5:"kaliya",
-            }
-        },
-    }
-]
+//                 name5:"orange",
+//             }
+//         },
+//         fastfood:
+//         {
+//             fastfood1:
+//             {
+//                item1:"panipuri",
+//             },
+//             fastfood2:
+//             {
+//                item2:"manchausup",
+//             },
+//             fastfood3:
+//             {
+//                item3:"pawbhaji",
+//             },
+//             fastfood4:
+//             {
+//                item4:"sendwich",
+//             },
+//             fastfood5:
+//             {
+//                item5:"dosa",
+//             }
+//         },
+//         color:
+//         {
+//             color1:
+//             {
+//                color1:"red",
+//             },
+//             color2:
+//             {
+//                color2:"blue",
+//             },
+//             color3:
+//             {
+//                color3:"yellow",
+//             },
+//             color4:
+//             {
+//                color:"green",
+//             },
+//             color5:
+//             {
+//                color5:"black",
+//             }
+//         },
+//         subtv:
+//         {
+//             subtv1:
+//             {
+//                person1:"jethalal",
+//             },
+//             subtv2:
+//             {
+//                 person2:"dayabhabhi",
+//             },
+//             subtv3:
+//             {
+//                 person3:"babitaji",
+//             },
+//             subtv4:
+//             {
+//                 person4:"champkchacha",
+//             },
+//             subtv5:
+//             {
+//                 person5:"dr.hathi",
+//             }
+//         },
+//         cartoon:
+//         {
+//             cartoon1:
+//             {
+//                c1:"chotabheem",
+//             },
+//             cartoon2:
+//             {
+//                c2:"raju",
+//             },
+//             cartoon3:
+//             {
+//                c3:"indumati",
+//             },
+//             cartoon4:
+//             {
+//                c4:"tuntunmosi",
+//             },
+//             cartoon5:
+//             {
+//                c5:"kaliya",
+//             }
+//         },
+//     }
+// ]
 
- const input = prompt("enter fruits or items:");
+//  const input = prompt("enter fruits or items:");
 
- if(input == " fruits"){
-    console.log("fruits",list[0]["fruits"]);
- }
- else if
-    (input == "fastfood"){
-        console.log("fastfood",list[0]["fastfood"]);
- }
- else if
-    (input == "color"){
-        console.log("color",list[0]["color"]);
- }
- else if
-    (input == "subtv"){
-        console.log("subtv",list[0]["subtv"]);
- }
- else if
-    (input == "cartoon"){
-        console.log("cartoon",list[0]["cartoon"]);
- }
-    else{
-        console.log("please! page is not found");
-    }
-}    
+//  if(input == " fruits"){
+//     console.log("fruits",list[0]["fruits"]);
+//  }
+//  else if
+//     (input == "fastfood"){
+//         console.log("fastfood",list[0]["fastfood"]);
+//  }
+//  else if
+//     (input == "color"){
+//         console.log("color",list[0]["color"]);
+//  }
+//  else if
+//     (input == "subtv"){
+//         console.log("subtv",list[0]["subtv"]);
+//  }
+//  else if
+//     (input == "cartoon"){
+//         console.log("cartoon",list[0]["cartoon"]);
+//  }
+//     else{
+//         console.log("please! page is not found");
+//     }
+// }    
     
 
 
