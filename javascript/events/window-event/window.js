fbffc93b42c7936a3e6c81c99ba8ae2075a87b85
Jasmin <jasmimulani@gmail.com>
2024-03-15 21:31:15 +0530

    15/03/24

---
+++ javascript/events/window-event/window.js
@@ -0,0 +1,25 @@
+//  function onclick(){
+//         document.getElementById("demo").innerHTML="javascript event";
+    
+//         document.getElementById("demo").style.backgroundColor = "red"
+//     }
+    
+//     /* add eventliseners*/
+    
+//     document.getElementById("head").addEventListener("mouseover",greet)
+    
+//     function greet(){
+//         document.getElementById("head").style.color ="orange"
+//     }
+/* close and open window */
+    {
+
+        // function openwindow(){
+        //      window.open()  // kaa to url thi thay ane kaa to _parant thi pn thay
+        // }
+   
+   
+        // function closewindow(){
+        //      window.close();
+        // }
+   }
