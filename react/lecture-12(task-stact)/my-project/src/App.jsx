da261ee5636404f6064c96b497b7c5eccf19ec9b
Jasmin <jasmimulani@gmail.com>
2024-04-12 11:14:39 +0530

    12/04/23

---
+++ react/lecture-12(task-stact)/my-project/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import Task_1 from './Component/Task_1'
+import Task_2 from './Component/Task_2'
+import Task_3 from './Component/Task_3'
+import Task_4 from './Component/Task_4'
+import Task_5 from './Component/Task_5'
+import Use_State from './Component/Use_State'
+
+function App() {
+   
+  return (
+    <>
+   <Use_State/>
+   <Task_1/>
+   <Task_2/>
+   <Task_3/>
+   <Task_4/>
+   <Task_5/>
+   
+  
+
+   
+    </>
+  )
+}
+
+export default App
