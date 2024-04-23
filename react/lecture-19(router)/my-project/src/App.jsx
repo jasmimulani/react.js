0cd6b0e89c5920fe5e5e40d3b3e019bf4707b1a7
Jasmin <jasmimulani@gmail.com>
2024-04-23 17:26:07 +0530

    23/4/24

--- react/lecture-19(router)/my-project/src/App.jsx
+++ react/lecture-19(router)/my-project/src/App.jsx
@@ -1,13 +1,26 @@
 import './App.css'
-import Home from './Componets/Home'
+import Home from './Pages/Home'
+import About from'./Pages/About'
+import Contact from'./Pages/Contact'
+import Pages from'./Pages/Pages'
+import Product from './Pages/Product'
+import { BrowserRouter ,Routes,Route } from 'react-router-dom'
+
 
 
 function App() {
- 
-
   return (
     <>
-  <Home/>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/product" element={<Product/>}/>
+      <Route path="/page" element={<Pages/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+    </Routes>
+    </BrowserRouter> 
+
     </>
   )
 }
