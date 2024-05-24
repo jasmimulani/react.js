0f79ec3e989a4f6d8f0785439fb9d49a5a527db5
Jasmin <jasmimulani@gmail.com>
2024-05-24 16:53:58 +0530

    24/05/24

--- react/lecture-27(add product)/my-project/src/ProductData/Redux/Reducer.jsx
+++ react/lecture-27(add product)/my-project/src/ProductData/Redux/Reducer.jsx
@@ -11,19 +11,28 @@ const getcartdatafromLocalstorage = () => {
 const Reducer = (state = getcartdatafromLocalstorage(),action) =>{
     switch(action.type){
         case ADD_TO_CART:
-            console.warn('add to cart called',action);
+            // console.warn('add to cart called',action);
             const updataerdcartAdd = [action.data , ...state]
             localStorage.setItem("cart" ,JSON.stringify(updataerdcartAdd))
             return updataerdcartAdd
 
+            const existingProduct = state.findIndex(item => item.id === action.data.id)
+            let updateCartData;
+            if(existingProduct >= 0){
+                updateCartData = state.map(item=>item.id === action.data.id ? {...item , quantity:item.quantity +1} : item)
+            }
+            else{
+                updateCartData = [{...action.data , quantity:1} , ...state]
+            }
+
             case REMOVE_TO_CART:
-                console.warn('remove to cart called',action);
+                // console.warn('remove to cart called',action);
                 const updataerdcartRemove = state.filter(item => item.id !== action.data)
                 localStorage.setItem("cart" ,JSON.stringify(updataerdcartRemove))
                 return updataerdcartRemove
 
                 case EMPTY_CART:
-                    console.warn("empty cart",action);
+                    // console.warn("empty cart",action);
                     localStorage.removeItem("cart")
                     return []
                     default:return state
