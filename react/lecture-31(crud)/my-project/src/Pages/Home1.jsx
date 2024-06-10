dac77037b80fb7e99ec159662550207eea08212a
Jasmin <jasmimulani@gmail.com>
2024-06-10 17:52:36 +0530

    10/06/24

--- react/lecture-31(crud)/my-project/src/Pages/Home1.jsx
+++ react/lecture-31(crud)/my-project/src/Pages/Home1.jsx
@@ -12,22 +12,27 @@ export function Home1() {
 
   const [searchTerm, setsearchTerm] = useState('')
   const [currentpage, setcurrentpage] = useState(1)
-  const [perpage, setperpage] = useState(2)
+  const [perpage, setperpage] = useState(1)
+
+
+  const filterData = data.filter(user => user.firstname.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    user.lastname.toLowerCase().includes(searchTerm.toLowerCase())
+  )
 
 
   const totalpages = Math.ceil(data.length / perpage)
 
 
-  const indexofLastData = currentpage + perpage
+  const indexofLastData = currentpage * perpage
   const indecofFristData = indexofLastData - perpage
-  const currentData = data.slice(indecofFristData,indexofLastData)
+  const currentData = filterData.slice(indecofFristData,indexofLastData)
 
   const peginate = (pagenumber) => {
     setcurrentpage(pagenumber)
   }
 
   const nextpage = () => {
-    setperpage((prevpage) => prevpage + 1)
+    setcurrentpage((prevpage) => Math.min(prevpage + 1 , totalpages))
   }
 
   const prevpage = () => { 
@@ -35,20 +40,17 @@ export function Home1() {
   }
 
   
+  useEffect(() => {
+    loaduser()
+  }, [])
 
   const loaduser = async () => {
     const res = await axios.get('http://localhost:3000/User')
     console.log(res.data, "users");
     setData(res.data)
   }
-  useEffect(() => {
-    loaduser()
-  }, [])
 
-  const filterData = data.filter(user => user.firstname.toLowerCase().includes(searchTerm.toLowerCase()) ||
-    user.lastname.toLowerCase().includes(searchTerm.toLowerCase())
-  )
-
+  
 
 
   const onDelete = (id) => {
@@ -57,13 +59,13 @@ export function Home1() {
         loaduser()
       })
       .catch((error) => {
-        console.log(error, 'error user data');
+        console.log(error, 'error user data');  
       })
   }
 
-  return (
+  return ( 
     <div>
-      <Navbar />
+      <Navbar searchTerm={searchTerm} setsearchTerm={setsearchTerm} />
       <section className="mx-auto w-full max-w-7xl px-4 py-4">
         <div className="flex flex-col space-y-4 md:flex-row md:items-center md:justify-between md:space-y-0">
           <div>
@@ -136,7 +138,7 @@ export function Home1() {
                       </th>
                     </tr>
                   </thead>
-                  {filterData.map((value) => {
+                  {currentData.map((value) => {
                     return (
                       <tbody className="divide-y divide-gray-200 bg-white" key={value.id}>
                         <tr>
@@ -194,6 +196,8 @@ export function Home1() {
               </div>
             </div>
           </div>
+          {
+            currentData.length == 0 ? null :(
           <div className="flex items-center justify-center pt-6">
             <Link className='mx-2 text-sm font-semibold text-gray-900'>
               <button onClick={prevpage} disabled={currentpage === 1}>
@@ -206,15 +210,21 @@ export function Home1() {
                 <NavLink className={`mx-1 flex justify-center items-center rounded-md border-gray-400 px-3 py-2 text-gray-300 hover:bg-black hover:text-white`} style={{
                   backgroundColor: currentpage == i + 1 ? 'black' : 'transparent', color: currentpage == i + 1 ? 'white' : 'inherit'
                 }}>
-                  <button key={i} onClick={() => peginate(i + 1)}>{i + 1}</button>
+                  <button key={i} onClick={() => peginate(i + 1)}>
+                    {i + 1}
+                  </button>
                 </NavLink>
               ))
             }
             <Link className='mx-2 text-sm font-semibold text-gray-900'>
+            <button onClick={nextpage}>
               <span>Next </span>
               <span>&rarr;</span>
+            </button>
             </Link>
           </div>
+            )
+          }
         </div>
       </section>
     </div>
