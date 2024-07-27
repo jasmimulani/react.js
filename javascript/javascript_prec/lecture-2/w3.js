3a1c55fb8f0888798e9515a48f8e01de5c4e5051
Jasmin <jasmimulani@gmail.com>
2024-07-27 18:06:20 +0530

    "27/07/24"

---
+++ javascript/javascript_prec/lecture-2/w3.js
@@ -0,0 +1,19 @@
+// --------------------------------------------------- w3school-----------------------
+
+//  function return
+
+
+let x = myfunction(4,3);
+
+function myfunction(a,b){
+    return a*b;
+}
+console.log(x);
+
+//  the () operator
+
+function tocelsius(faranheit){
+    return(5/9) * (faranheit -32);
+}
+let value = tocelsius(77);
+console.log(value);
